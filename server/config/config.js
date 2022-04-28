a4b1b7175bd75c6ce0ef27bee15888c4a5b1ea0a
Petr <ptrklsnkv@gmail.com>
2022-04-28 13:53:52 +0300

    create sessionConfig

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const configSession = require('./configSession');
+
+const config = (app) => {
+
+};
