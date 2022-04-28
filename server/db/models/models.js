fbad37b7fc4189b43f42c947e82e585dcfbd44e0
Den <ndvofc@gmail.com>
2022-04-28 19:36:08 +0300

    database st2

---
+++ server/db/models/models.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  progress: { type: DataTypes.INTEGER, defaultValue: 0 },
+});
+
+const JSmodule = sequelize.define("JSmodule", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  titleThemes: { type: DataTypes.STRING },
+});
+
+module.exports = {
+  User,
+  JSmodule,
+};
