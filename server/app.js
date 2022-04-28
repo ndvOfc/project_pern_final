9501168a4fc3f14d9c46ae85ec33e6f67a985c6d
Petr <ptrklsnkv@gmail.com>
2022-04-28 19:28:20 +0300

    do commit before update

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const config = require('./config/config');
 const router = require('./routes/mainRouter');
-const sequelize = require('./db/db');
 
 const app = express();
 const PORT = process.env.PORT ?? 5001;
@@ -11,10 +10,8 @@ config(app);
 
 app.use('/api', router);
 
-const start = async () => {
+const start = () => {
   try {
-    await sequelize.authenticate();
-    await sequelize.sync();
     // await sequelize.sync({ force: true });
     app.listen(PORT, () => console.log(`*** Шуршит маленький на пору ${PORT} ***`));
   } catch (e) {
