70d8d20fcdc192c304a1568c5d40462df6ea2ff6
Stepashka <foraudo@mail.ru>
2022-04-28 20:40:04 +0300

    Registration & Login + useForm

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { Grid, Paper, TextField, Button, Typography, Link } from '@material-ui/core';
+import { Grid, Paper, Button, TextField, Typography, Link } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
 
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
 // import Checkbox from '@material-ui/core/Checkbox';
@@ -8,17 +11,36 @@ import { Grid, Paper, TextField, Button, Typography, Link } from '@material-ui/c
 function Login() {
   const paperStyle = { padding: 20, width: 300, margin: '20px auto' };
   const btnstyle = { margin: '8px 0' };
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
         <Grid align="center">
           <h2>Войти</h2>
         </Grid>
-        <TextField label="Имя пользователя" placeholder="Имя пользователя" fullWidth />
-        <TextField label="Пароль" placeholder="Введите пароль" type="password" fullWidth />
-        <Button type="submit" color="primary" variant="contained" style={btnstyle} fullWidth>
-          Войти
-        </Button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            {...register('username', { required: true })}
+            label="Имя пользователя"
+            placeholder="Имя пользователя"
+            type="text"
+            fullWidth
+          />
+          <TextField
+            {...register('password', { required: true })}
+            label="Пароль"
+            placeholder="Введите пароль"
+            type="password"
+            fullWidth
+          />
+          <Button type="submit" color="primary" variant="contained" style={btnstyle} fullWidth>
+            Войти
+          </Button>
+        </form>
+
         <Typography>
           <Link href="#">Забыли пароль ?</Link>
         </Typography>
